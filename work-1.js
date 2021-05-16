cb4ee3b04fdbe240c47afe18980dcc7beb15dc86
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2021-05-16 21:36:04 +0530

    Accessing of various values using console

---
+++ work-1.js
@@ -0,0 +1,23 @@
+const uid = 'abc@123';
+var firstName = "Jibin";
+var lastName = "Joseph";
+var email = "jibinjoseph2000@gmail.com";
+var country = "India";
+var state = "Kerala";
+var password = "jibin@123";
+var courseCount = 0;
+var isLoggedInFromEmail = true;
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFaceboook = false;
+
+console.log(`
+    With Unique Id: ${uid}
+    User Name is: ${firstName} ${lastName}
+    and his email is: ${email}
+    and uses the password: ${password}
+    and Logged using email: ${isLoggedInFromEmail}
+`);
+
+console.log(`
+    User Entry verified
+`);
